14fe2f661ee5c6f6a983e5609ffc5d114ad0b8b7
andrea tramacere <orion@MacBook-Pro-3.local>
2018-11-15 17:54:15 +0100

    added proper error message for no data

--- cdci_polar_plugin/polar_lightcurve_query.py
+++ cdci_polar_plugin/polar_lightcurve_query.py
@@ -136,7 +136,7 @@ class PolarLigthtCurve(LightCurveProduct):
 
         else:
             #print("result",res) # logging?
-            _d=res_json['status']['success']
+            _d=res_json['status']
             raise PolarAnalysisException(message='polar light curve failed: %s'%_d['comment'],debug_message=_d['kind'])
 
 
